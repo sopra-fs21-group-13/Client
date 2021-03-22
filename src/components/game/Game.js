e78ab1b18f7339d6caf1a5ce223b4e80796d1950
NazimBayram <56076114+NazimBayram@users.noreply.github.com>
2021-03-22 20:49:34 +0100

    Update Game.js
    Test 22.03 :)

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -7,6 +7,8 @@ import { Spinner } from '../../views/design/Spinner';
 import { Button } from '../../views/design/Button';
 import { withRouter } from 'react-router-dom';
 
+//TEST 22.03.21
+
 const Container = styled(BaseContainer)`
   color: white;
   text-align: center;
