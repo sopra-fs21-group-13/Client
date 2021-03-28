ff798c4828b9c9d79cce9e76169af308339d102b
NazimBayram <56076114+NazimBayram@users.noreply.github.com>
2021-03-28 23:05:50 +0200

    Update getDomain.js

--- src/helpers/getDomain.js
+++ src/helpers/getDomain.js
@@ -7,10 +7,8 @@ import { isProduction } from './isProduction';
  * @returns {string}
  */
 export const getDomain = () => {
-  //const prodUrl = 'https://sopra-fs21-group-13-server.herokuapp.com'; // TODO: insert your groups heroku prod url for server (once deployed)
-  const prodUrl = 'https://group13-server.herokuapp.com/';
-  //const devUrl = 'http://localhost:8080';
-  const devUrl = 'https://group13-server.herokuapp.com/';
+  const prodUrl = 'https://sopra-fs21-group-13-server.herokuapp.com'; // TODO: insert your groups heroku prod url for server (once deployed)
+  const devUrl = 'http://localhost:8080';
 
   return isProduction() ? prodUrl : devUrl;
 };
