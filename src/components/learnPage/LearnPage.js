a3163ca5084b88b1b54a9ab507c9394ccc871f72
Remus <remus.nichiteanu@uzh.ch>
2021-04-02 16:00:39 +0200

    Worked on the LearnPage layout
    - added Set info
    
    -added left and right buttons without functionality

--- src/components/learnPage/LearnPage.js
+++ src/components/learnPage/LearnPage.js
@@ -3,8 +3,9 @@ import styled from 'styled-components';
 import { BaseContainer } from '../../helpers/layout';
 import { api, handleError } from '../../helpers/api';
 import CardRender from '../../views/design/CardRender';
-import Card from 'C:/Users/Conrad/Documents/GitHub/Client/src/components/shared/models/Card';
+import Card from '../shared/models/Card';
 import { withRouter } from 'react-router-dom';
+import './learnPage.css'
 
 const Container = styled(BaseContainer)`
     color: black;
@@ -13,20 +14,59 @@ const Container = styled(BaseContainer)`
 
 const Header = styled.div`
     position: sticky;
-    height: 50px;
+    height: 60px;
     text-align: center;
     color:white;
-    background-color:green;
+    background-color:#70F0A9;
     top: 0px;
 `
 
 const MainContainer = styled.div`
     display: flex;
     justify-content: center;
+    padding: 10px;
+    background-color: white;
+    height: 70vh;
+
+`
+
+const CardContainer = styled.div`
+    display: flex;
+    justify-content: center;
     align-items: center;
-    padding: 20px;
-    background-color: red;
-    height: 130vh;
+    padding: 10px;
+    height: 60vh;
+    width: 200vh;
+`
+
+const InfoContainer = styled.div`
+    display: relative;
+    align-items: top;
+    position: relative;
+    align-self: center;
+    height: 300px;
+    background-color: white;
+
+`
+
+const Info = styled.div`
+    align-items: center;
+    position: absolute;
+    height: 200px;
+    width: 900px;
+    left: 50px;
+    bottom: 20px;
+`
+
+
+
+const Footer = styled.div`
+    position: relative;
+    height: 100px;
+    text-align: center;
+    color:white;
+    background-color:#70F0A9;
+    bottom:0px;
 `
 
 
@@ -34,6 +74,7 @@ class LearnPage extends React.Component {
     constructor() {
       super();
       this.state = {
+          set: null,
           flashcards: null,
           currentFlashcard: null
       };
@@ -42,6 +83,13 @@ class LearnPage extends React.Component {
     async componentDidMount(){
 
         const flashcard1 = new Card({id: 1, question: "Area", answer: "Fläche"});
+
+        const example_set = [
+            "Business English",
+            "This set is for people that want to learn some business english. Study well, live well.",
+            "FlashyBoss2003",
+            32
+        ]
         
 
         const response = [
@@ -54,7 +102,7 @@ class LearnPage extends React.Component {
                 question: "Business"
             }]
 
-        this.setState({flashcards: response});
+        this.setState({flashcards: response, set: example_set});
         //Here we could save the currentFlashcard id inside the user
         //and then reenter it here inside a variable for saving the current state
         //(which card was last shown when closing the application etc)
@@ -64,17 +112,71 @@ class LearnPage extends React.Component {
     render(){
         return(
             <div>
-            <Header>
-                header
-            </Header>
+            <Header>Header</Header>
+            <InfoContainer>
+                <Info>
+                    <div class = "info-grid">
+                        <div class = "info-block info-block-1">
+                        {!this.state.flashcards ? (
+            
+                        <div>Name</div>
+                        ) : (
+                            <div class = "name-grid">
+                                <div class = "set-name">
+                                    {this.state.set[0]}
+                                </div>
+                                <div class = "creator-name">
+                                    {this.state.set[2]}
+                                </div>
+                            </div>
+                        )
+                        }
+                        </div>
+                        <div class = "info-block info-block-2">
+                        {!this.state.flashcards ? (
+            
+                        <div>Name</div>
+                        ) : (
+                            this.state.set[1]
+                        )
+                        }
+                        </div>
+                        <div class = "info-block info-block-3">
+                        {!this.state.flashcards ? (
+            
+                        <div>Name</div>
+                        ) : (
+                            this.state.set[3]
+                        )
+                        }
+                        </div>
+                    </div>
+                </Info>
+            </InfoContainer>
             <MainContainer>
-            {!this.state.flashcards ? (
-            <div>loading..</div>
-            ) : (
-                <CardRender flashcard = {this.state.currentFlashcard}
-                key = {this.state.currentFlashcard.id} />
-            )}
+                <CardContainer>
+                    
+                {!this.state.flashcards ? (
+                
+                <div>Loading..</div>
+                ) : (
+                    <div>
+                    <CardRender flashcard = {this.state.currentFlashcard}
+                    key = {this.state.currentFlashcard.id} />
+                        <div class = "button-container">
+                            <div class = "left-button-container">
+                            <button>left</button>
+                            </div>
+                            <div class = "right-button-container">
+                            <button  >right</button>
+                            </div>
+                        </div>
+                    </div>
+                )}
+                </CardContainer>
+                
             </MainContainer>
+            <Footer>Footer</Footer>
             </div>
         );
     }
