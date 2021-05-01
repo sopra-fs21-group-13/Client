044982e6cb8a221cf9ac937322831b458884c8be
rnichi1 <remus.nichiteanu@uzh.ch>
2021-05-01 15:58:10 +0200

    Profile pictures show in "search users"

--- src/components/search/searchUsers/SearchUsers.js
+++ src/components/search/searchUsers/SearchUsers.js
@@ -4,6 +4,24 @@ import { useHistory } from "react-router-dom";
 import SideFilter from '../sideFilter/SideFilter';
 import FriendshipBtn from '../friendship/FriendshipBtn.js'
 
+//profile pictures
+import char1 from "../../profile/char1.jpg";
+import char2 from "../../profile/char2.jpg";
+import char3 from "../../profile/char3.jpg";
+import char4 from "../../profile/char4.jpg";
+import char5 from "../../profile/char5.jpg";
+import char6 from "../../profile/char6.jpg";
+import char7 from "../../profile/char7.jpg";
+import char8 from "../../profile/char8.jpg";
+import char9 from "../../profile/char9.jpg";
+import char10 from "../../profile/char10.jpg";
+import char11 from "../../profile/char11.jpg";
+import char12 from "../../profile/char12.jpg";
+import char13 from "../../profile/char13.jpg";
+import char14 from "../../profile/char14.jpg";
+import char15 from "../../profile/char15.jpg";
+import char16 from "../../profile/char16.jpg";
+
 //css
 import './searchUsers.css';
 
@@ -21,17 +39,55 @@ import { api, handleError } from "../../../helpers/api";
 
 function SearchUsers(props){
 
-    const [allUsers, setAllUsers] = useState([]);
+    //handles showing the right user picture
+    const [userPicturesDict, setUserPicturesDict] = useState({1: char1, 2: char2, 3: char3, 4: char4, 5: char5, 6: char6,
+        7: char7, 8: char8, 9: char9,10: char10, 11: char11, 12: char12,13: char13, 14: char14, 15: char15, 16: char16});
+    const [currentPics, setCurrentPics] = useState();
+
+    //amount of likes all user have across all their sets.
+    const[likes, setLikes] = useState();
+
+    const [allUsers, setAllUsers] = useState();
 
     useEffect(() => {
         api.get("/users").then(response => {
             setAllUsers(response.data);
             console.log("hello",allUsers);
 
+            setLikesAndPics(response.data)
         }).catch(e=>{
             alert(`Something went wrong while fetching all users: \n${handleError(e)}`);
         })
     }, []) 
+
+    function setLikesAndPics(users){
+        
+        var picsDict = {};
+        var likesDict = {};
+
+        //set the right picture and like count for every user 
+        for (var i = 0; i<users.length; i++){
+            console.log(i)
+            //current user that is looked at
+            const user=users[i]
+            //sets pics of users in state
+            picsDict[user.userId] = userPicturesDict[Number(user.photo)];
+
+            //calculates likes 
+            var likes = 0;
+            for(var j=0; j<user.learnSets.length; j++){
+                if(user.learnSets[i].likes == null){
+                    user.learnSets[i].likes = 0
+                }
+                likes = likes + user.learnSets[i].likes
+            }
+            //sets likes in state
+            likesDict[user.userId] = likes;
+        }
+
+        setLikes(likesDict);
+        setCurrentPics(picsDict);
+    }
 
     /*useEffect(() => {
         history.push("dashboard")
@@ -71,7 +127,7 @@ function SearchUsers(props){
 
                 <SideFilter view="users"/>
 
-                <div id="result_board">
+                {(!currentPics || !likes) ? (<div>loading users</div>) : (<div id="result_board">
                     <div id="board_title"> {/*this should be changeable */}
                         <h1>All Users</h1>
                     </div>
@@ -83,7 +139,7 @@ function SearchUsers(props){
 
                             <div class="userBasic">
                                 <div class="photoFrame">
-                                    <img src={user.photo} />                            
+                                    <img src={currentPics[user.userId]} />                            
                                 </div>
                                 <p class="profile_username">
                                     {user.username}
@@ -92,7 +148,7 @@ function SearchUsers(props){
 
                             <div class="userMore">
                             <p class="likes_wins">
-                                <span class="thumbIcon"><ThumbUpAltOutlinedIcon/></span> {user.likes} <span class="winIcon"><EmojiEventsIcon/></span> {user.wins}
+                                <span class="thumbIcon"><ThumbUpAltOutlinedIcon/></span> {likes[user.userId]} <span class="winIcon"><EmojiEventsIcon/></span> {user.wins}
                             </p>
                             <p>
                                 {user.username}
@@ -109,7 +165,8 @@ function SearchUsers(props){
 
                         ))}
                     </div>
-                </div>
+                </div>) }
+                
                 
 
             </div>
