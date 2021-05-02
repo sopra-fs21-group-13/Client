70de5ffcc925bd2c678396d22a646f2780e3ecbd
sicadu <43454522+sicadu@users.noreply.github.com>
2021-05-02 16:01:53 +0200

    Update getDomain.js

--- src/helpers/getDomain.js
+++ src/helpers/getDomain.js
@@ -7,7 +7,7 @@ import { isProduction } from './isProduction';
  * @returns {string}
  */
 export const getDomain = () => {
-  const prodUrl = 'http://localhost:8080'; // TODO: insert your groups heroku prod url for server (once deployed)
+  const prodUrl = 'https://sopra-fs21-group-13-server.herokuapp.com/'; // TODO: insert your groups heroku prod url for server (once deployed)
   const devUrl = 'http://localhost:8080';
 
   return isProduction() ? prodUrl : devUrl;
