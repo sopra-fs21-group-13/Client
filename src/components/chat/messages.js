24a8ffbab16763f7f7656e9a287337432ab847ab
SoftwareConstructionGroup <kiram.benaleya@uzh.ch>
2021-05-29 17:26:19 +0200

    Update messages.js

--- src/components/chat/messages.js
+++ src/components/chat/messages.js
@@ -9,71 +9,11 @@ class Messages extends React.Component {
         super(props);
         this.state = {
             messages: null,
-            users: [
-            {
-              senderID:123,
-              timeStamp: "13:00:01 12.05.21",
-              message: "Lol u suck ass!"
-            },
-            {
-              senderID:23,
-              timeStamp: "13:00:03 12.05.21",
-              message: "No problem I will restart the application thank you very much i lie a lot and so do u"
-            },
-            {
-               senderID:123,
-               timeStamp: "13:00:12 12.05.21",
-               message: "lon denner denses den lorem ipsum sim dender kan nimar denb bunde rellad!."
-            },
-            {
-              senderID:123,
-              timeStamp: "13:00:01 12.05.21",
-              message: "Lol u suck ass!"
-            },
-            {
-              senderID:23,
-              timeStamp: "13:00:03 12.05.21",
-              message: "xD"
-            },
-            {
-               senderID:123,
-               timeStamp: "13:00:12 12.05.21",
-               message: "lorem ipsum sim dender kan nimar denb bunde rellad."
-            },
-            {
-              senderID:123,
-              timeStamp: "13:00:01 12.05.21",
-              message: "lon denner denses den lorem ipsum sim dender kan nimar denb bunde rellad!"
-            },
-            {
-              senderID:23,
-              timeStamp: "13:00:03 12.05.21",
-              message: "lorem ipsum sim dender kan nimar denb bunde rellad nado kann dem dener:)"
-            },
-            {
-               senderID:123,
-               timeStamp: "13:00:12 12.05.21",
-               message: "Jesus chirst chill tf out man."
-            },
-            {
-              senderID:123,
-              timeStamp: "13:00:01 12.05.21",
-              message: "Lol!"
-            },
-            {
-              senderID:23,
-              timeStamp: "13:00:03 12.05.21",
-              message: "okay :)"
-            },
-            {
-               senderID:123,
-               timeStamp: "13:00:12 12.05.21",
-               message: "Jesus chirst chill tf out man."
-            }]
         };
     }
 
-    async loadMessages() {
+    async update() {
+    // the local storage bit needs to be changed to gameId (passed down by the game component
         const response = await api.get("/games/" + localStorage.getItem("userId"));
         const data = await response.json();
         this.setState({messages: data.histories});
@@ -84,29 +24,44 @@ class Messages extends React.Component {
     scrollToRef() {
         const scroll = this.chatContainer.current.scrollHeight - this.chatContainer.current.clientHeight;
         this.chatContainer.current.scrollTo(0, scroll);
-  };
-
-    startInterval() {
-        setInterval(this.loadMessages(),1000);
-       // setTimeout(this.loadMessages(), 1000);
     };
 
-    mapMessages() {
+   componentDidMount() {
+      setInterval(this.update(),1000);
+   };
 
+   componentWillUnmount(){
+       clearInterval();
+   };
+
+    mapMessages() {
+        // map messages to array so they can be displayed
+        if (this.state.messages != null){
+            return (
+                <div>
+                    {this.state.messages.map((item, index)=>
+                      <div className="textContainer" ref={this.chatContainer}>
+                        {this.state.users.map((item, index)=>
+                          <div>
+                            <div className="id">{item.senderID} &nbsp;</div>
+                            <div className="message">{item.message} &nbsp;</div>
+                            <div className="timestamp"> {item.timeStamp}</div>
+                          </div>
+                        )}
+                    )}
+                </div>
+            );
+        }
+        else {
+            return(
+                <div className="text">You don't have any invitations! </div>
+            );
+        }
     };
 
     render() {
-      this.startInterval();
         return (
-            <div className="textContainer" ref={this.chatContainer}>
-                {this.state.users.map((item, index)=>
-                    <div>
-                    <div className="id">{item.senderID} &nbsp;</div>
-                    <div className="message">{item.message} &nbsp;</div>
-                    <div className="timestamp"> {item.timeStamp}</div>
-                    </div>
-                )}
-            </div>
+            this.mapMessages();
         );
     }
 }
