da6881448f7c2f52c91eb4fb1691910b2176315f
sicadu <43454522+sicadu@users.noreply.github.com>
2021-05-30 22:55:36 +0200

    invitation send, picture add

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -8,31 +8,23 @@ import User from '../shared/models/User';
 import { useHistory, useLocation, withRouter } from 'react-router-dom';
 import Invite from '../InviteHeader/invite.js'
 
-export const users = [
-    {
-        username:"realUnicorn",
-        info: "Rainbows are mine",
-        likes: 205,
-        wins: 30,
-        photo:"https://images-na.ssl-images-amazon.com/images/I/51mBDeh7PcL.jpg"
-    },
-    {
-        username:"Squirrel",
-        info: "Where are more nuts??",
-        likes: 23,
-        wins: 31,
-        photo:"https://i.pinimg.com/originals/ea/53/fd/ea53fdd77bfcf158b3015ed93ab39d8a.jpg"
-    },
-    {
-        username:"Kitty",
-        info: "I want to be alone",
-        likes: 1000,
-        wins: 1,
-        photo:"https://www.meme-arsenal.com/memes/f04ebf47a09312cbedfca22256c5722d.jpg"
-}]
-
-
-
+//profile pictures
+import char1 from "../profile/char1.jpg";
+import char2 from "../profile/char2.jpg";
+import char3 from "../profile/char3.jpg";
+import char4 from "../profile/char4.jpg";
+import char5 from "../profile/char5.jpg";
+import char6 from "../profile/char6.jpg";
+import char7 from "../profile/char7.jpg";
+import char8 from "../profile/char8.jpg";
+import char9 from "../profile/char9.jpg";
+import char10 from "../profile/char10.jpg";
+import char11 from "../profile/char11.jpg";
+import char12 from "../profile/char12.jpg";
+import char13 from "../profile/char13.jpg";
+import char14 from "../profile/char14.jpg";
+import char15 from "../profile/char15.jpg";
+import char16 from "../profile/char16.jpg";
 
 //export const /*erase if not working*/ 
 function Modal({ handleClose, show, children, currentWindow, members,mainPageModalTypeSetter, clickUsers, ...props }){
@@ -45,6 +37,11 @@ function Modal({ handleClose, show, children, currentWindow, members,mainPageMod
     const [invitedPlayers, setInvitedPlayers] = useState([]);
     const [lastTime, setLastTime] = useState(30);
     const [lastCard, setLastCard] = useState(10);
+
+    //handles showing the right user picture
+    const [userPicturesDict, setUserPicturesDict] = useState({1: char1, 2: char2, 3: char3, 4: char4, 5: char5, 6: char6,
+        7: char7, 8: char8, 9: char9,10: char10, 11: char11, 12: char12,13: char13, 14: char14, 15: char15, 16: char16});
+    const [currentPic, setCurrentPic] = useState();
 
     let history = useHistory();
 
@@ -60,24 +57,7 @@ function Modal({ handleClose, show, children, currentWindow, members,mainPageMod
         {value: 30}
     ]);
 
-    /*
-    useEffect(() => {
-        setSuitableUsers(clickUsers);
-    }, [])
-    */
-
     useEffect(() => { 
-        
-        /*
-        var suitableUsersCopy = [...suitableUsers];
-        var correctUsers = [];
-        for (var i= 0; i < suitableUsersCopy.length; i++){
-            if (suitableUsersCopy[i].userId != localStorage.getItem('userId')){
-                correctUsers.push(clickUsers[i])
-            }
-        }
-        */
-
         setSuitableUsers(clickUsers);
         setInvitedPlayers([]);
     },[clickUsers])
@@ -126,7 +106,6 @@ function Modal({ handleClose, show, children, currentWindow, members,mainPageMod
                 });
             
             await api.post('/games/invitations', invitation);
-            //handleClose();
             history.push('/game/' + Number(gameResponse.data.gameId));
 
         } catch (error){
@@ -250,7 +229,7 @@ console.log("current window:", currentWindow)
                             className={invitedPlayers.includes(user) ? 'userCardModalSelected' : 'userCardModal'} >
                             <div class="userBasic">
                                 <div class="photoFrame">
-                                    <img src={user.photo} />                            
+                                    <img src={userPicturesDict[Number(user.photo)]} />                            
                                 </div>
                                 <p class="profile_username">
                                     {user.username}
@@ -258,7 +237,7 @@ console.log("current window:", currentWindow)
                             </div>
                             <div class="userMore">
                                 <p class="likes_wins">
-                                    <span class="thumbIcon"><ThumbUpAltOutlinedIcon/></span> {user.userId}
+                                    <span class="thumbIcon"><ThumbUpAltOutlinedIcon/></span> {user.liked}
                                     <span class="winIcon"><EmojiEventsIcon/></span> {user.numberOfWins}
                                 </p>
                                 <p>
@@ -267,7 +246,6 @@ console.log("current window:", currentWindow)
                             </div>
                             <div class="invitationButton"> INVITE </div>
                         </div>
-                        
                     </div>
                     ))}
                 {/* 
